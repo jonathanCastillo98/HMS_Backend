223569b422450d3d8daedafec8064975725b0c2c
JonathanCastillo-Ksquare <Jonathan.Castillo@itksquare.edu.mx>
2023-03-31 17:27:15 -0600

    latest

--- dist/app.js
+++ dist/app.js
@@ -5,8 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const routes_1 = __importDefault(require("./routes"));
+const cors = require("cors");
 const app = (0, express_1.default)();
 // Middlewares
 app.use(express_1.default.json());
+app.use(cors);
 app.use('/', routes_1.default);
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express';
 import router from './routes';
+const cors = require("cors")
 
 const app = express();
 
 // Middlewares
 app.use(express.json())
+app.use(cors)
 app.use('/', router);
 
 export default app;
